232b7e4d8c675062536db0b48e7fcba67aebd663
mdasifalijihat1 <mdasifalijihat1@gmail.com>
2024-09-18 22:58:15 +0600

    added all file

--- js/pay.js
+++ js/pay.js
@@ -2,13 +2,41 @@
 // search:  form submit reloading the page 
 
 // step - 1 set event handler
-document.getElementById('btn-login').addEventListener('click', function(event){
-    // step-2: prevent dafauult vehabior (reloading browser)
-    event.preventDefault();  
-    console.log('login btn clicked');
+// document.getElementById('btn-login').addEventListener('click', function(event){
+//     // step-2: prevent dafauult vehabior (reloading browser)
+//     event.preventDefault();  
+//     console.log('login btn clicked');
 
-    // step-3: get the phone number
-    const phoneNumber =document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+//     // step-3: get the phone number
+//     const phoneNumber =document.getElementById('phone-number').value;
+//     const pinNumber =document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber);
+//     // step-4 validate phone and pin
+// // this is temporary. you should do like this 
+//     if(phoneNumber=== '5' && pinNumber= '1234'){
+//         console.log('your are loddend in');
+//         // setp -5: allow user to use the website
+//     }
+//     else{
+//         alert('wrong number')
+//     }
     
+// })
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault();
+    //  get phone number add pin
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+    if(phoneNumber === '5' && pinNumber ==='1234'){
+        console.log('your right number');
+        // home page login way
+        window.location.href = '/home.html'
+        
+    }
+    else{
+        alert('wrong number ')
+    }
+    
+
 })
